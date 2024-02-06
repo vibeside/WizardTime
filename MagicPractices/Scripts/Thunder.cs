5b02fb15981ff1102d6150bf513a0114558e659f
vibeside <vibeside01@gmail.com>
2024-02-05 20:56:33 -0500

    I am a foolish wizard, and failed my conjuration. Another attempt to conjure my spells!

---
+++ MagicPractices/Scripts/Thunder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace WizardTime.Scripts
+{
+    internal class Thunder : MonoBehaviour
+    {
+    }
+}
