c75413f4240a8cc783046da03cbfbfd35da963a2
vibeside <vibeside01@gmail.com>
2024-02-05 20:55:54 -0500

    Conjured up the spells beginnings!

--- MagicPractices/WizardTimePlugin.cs
+++ MagicPractices/WizardTimePlugin.cs
@@ -2,7 +2,9 @@
 using BepInEx.Logging;
 using MonoMod.RuntimeDetour;
 using System;
+using System.IO;
 using System.Reflection;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace WizardTime
@@ -11,22 +13,32 @@ namespace WizardTime
     public class WizardTimePlugin : BaseUnityPlugin
     {
         public const string modGUID = "grug.lethalcompany.WizardTime";
-        public const string modName = "MagicPracices";
+        public const string modName = "MagicPractices";
         public const string modVersion = "0.1.0.0";
         public static ManualLogSource? mls;
         public static GameObject? focusOrb;
+        private (uint, uint, uint, uint) QuadHash(int SALT = 0)
+        { // [!code ++]
+            Hash128 longHash = new Hash128();
+            longHash.Append(modGUID);
+            longHash.Append(SALT);
+            return ((uint)longHash.u64_0, (uint)(longHash.u64_0 >> 32),
+                    (uint)longHash.u64_1, (uint)(longHash.u64_1 >> 32));
+        }
         public void Awake()
         {
             mls = base.Logger;
             //Holds the scripts used by the mod to do magic!
             focusOrb = new GameObject("Magical Glowing Orb");
+            focusOrb.AddComponent<NetworkObject>();
+            var (hash, _, _, _) = QuadHash(0);
+            focusOrb.GetComponent<NetworkObject>().GlobalObjectIdHash = hash;
             //new Hook(
             //typeof(GameNetworkManager).GetMethod("Start", (BindingFlags)int.MaxValue),
             //(Action<GameNetworkManager> original, GameNetworkManager self) =>
             //{
             //    original(self);
             //});
-
         }
     }
 }
