bb1c3f9aae9c387331a295dfdac06fc506079e4f
vibeside <vibeside01@gmail.com>
2024-02-16 19:09:17 -0500

    Created spellbooks and tomes

---
+++ MagicPractices/Scripts/SpellBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace WizardTime.Scripts
+{
+    internal class SpellBook : MonoBehaviour
+    {
+        public Fire? fireKnowledge;
+        public Blood? bloodKnowledge;
+        public Thunder? thunderKnowledge;
+        public Gravity? gravityKnowledge;
+    }
+}
---
+++ MagicPractices/Scripts/Tome.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace WizardTime.Scripts
+{
+    internal class Tome : MonoBehaviour
+    {
+        public Spell? minorMagicks;
+        public Spell? majorMagicks;
+        public Spell? buffMagicks;
+        public Spell? defensiveMagicks;
+        public virtual void FireSpell(Spell firedSpell)
+        {
+            WizardTimePlugin.mls.LogInfo("fired a spell, loser");
+        }
+    }
+}
